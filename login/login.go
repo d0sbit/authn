7ac54bb3d8b85c2242c647285539592928f59fcf
Brad Peabody <brad@peabody.io>
2020-07-12 17:08:34 -0700

    some more setup

--- login/login.go
+++ login/login.go
@@ -1,31 +1,70 @@
+// Package login provides simple username and password login functionality.
 package login
 
 import "net/http"
 
+// New returns a new initialized Handler.
+func New(lc Checker, lw Writer) *Handler {
+	return &Handler{
+		lc: lc,
+		lw: lw,
+	}
+}
+
+// Handler implements login API calls.
 type Handler struct {
-	Provider
+	lc Checker
+	lw Writer
 }
 
-type Provider interface {
-	TryLogin(username, password string) (AuthnIDer, error)
+// Checker can check logins to see if they are valid and if so return the key.
+type Checker interface {
+	LoginCheck(username, password string) (Keyer, error)
 }
 
-type AuthnIDer interface {
-	AuthnID() string
+// CheckerFunc implements Checker as a function
+type CheckerFunc func(username, password string) (Keyer, error)
+
+// LoginCheck implements Checker
+func (f CheckerFunc) LoginCheck(username, password string) (Keyer, error) {
+	return f(username, password)
 }
 
-// AuthnIDString implements AuthnIDer as a string.
-type AuthnIDString string
+// Keyer has a LoginKey method which is the unique ID associated with a login.
+type Keyer interface {
+	LoginKey() string // unique ID associated with login
+}
 
-// AuthnID implements AuthnIDer.
-func (s AuthnIDString) AuthnID() string {
+// KeyString implements Keyer as a string.
+type KeyString string
+
+// LoginKey implements Keyer.
+func (s KeyString) LoginKey() string {
 	return string(s)
 }
 
-type LoginReader interface {
-	ReadLogin(r *http.Request, w http.ResponseWriter) (AuthnIDer, error)
+// Reader knows how to read login information from a request.
+type Reader interface {
+	LoginRead(r *http.Request, w http.ResponseWriter) (Keyer, error)
 }
 
-type LoginWriter interface {
-	WriteLogin(r *http.Request, w http.ResponseWriter, a AuthnIDer) error
+// ReaderFunc implements Reader as a function.
+type ReaderFunc func(r *http.Request, w http.ResponseWriter) (Keyer, error)
+
+// LoginRead implements Reader
+func (f ReaderFunc) LoginRead(r *http.Request, w http.ResponseWriter) (Keyer, error) {
+	return f(r, w)
+}
+
+// Writer knows how to write a login key to be associated with a request.
+type Writer interface {
+	LoginWrite(r *http.Request, w http.ResponseWriter, k Keyer) error
+}
+
+// WriterFunc implements Writer as a function.
+type WriterFunc func(r *http.Request, w http.ResponseWriter, k Keyer) error
+
+// LoginWrite implements Writer
+func (f WriterFunc) LoginWrite(r *http.Request, w http.ResponseWriter, k Keyer) error {
+	return f(r, w, k)
 }
--- login/login_test.go
+++ login/login_test.go
@@ -1,1 +1,18 @@
 package login
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLogin(t *testing.T) {
+
+}
+
+func ExampleHandler() {
+
+	h := New(nil, nil)
+	fmt.Printf("HERE: %p", h)
+
+	// Output: blah
+}
